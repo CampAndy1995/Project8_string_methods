05719faba347035bbc7182b3f47305e2054fd7e7
CampAndy1995 <144943158+CampAndy1995@users.noreply.github.com>
2023-12-14 14:19:15 -0800

    first commit

---
+++ JS/main.js
@@ -0,0 +1,8 @@
+const str1 = "Hello";
+const str2 = "Andrew";
+
+const greeting = str1.concat(str2);
+
+
+// -- Cannot get js file to link to html file -- //
+//Javascript is written with html file // 
